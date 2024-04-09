bd917991a33350a0936860db294ddd0f3a30ed37
MSI\gn01792218 <gn01792218@hotmail.com>
2024-04-10 00:51:49 +0800

    [modify] modify pattern selectors

--- components/BetSetSelector.vue
+++ components/BetSetSelector.vue
@@ -1,11 +1,7 @@
 <template>
-    <div class="relative flex flex-col items-center" >
-        <p class="cursor-pointer" @click="toggleMenum">套路{{ patternsShowText }}</p>
-        <ul class="p-2 absolute z-[10] top-[25px] bg-black text-white w-full" v-show="showMenum">
-            <li class="w-full hover:bg-slate-700" v-for="[patternKey,value] in Object.entries(patterns)" :key="patternKey" @click="setPattern(patternKey, value)">
-                {{ value }}
-            </li>
-        </ul>
+    <div class="relative flex items-center">
+        <p class="cursor-pointer">套路</p>
+        <USelect v-model="patternsShowText" :options="Object.values(patterns)" @change="setPattern" />
     </div>
 </template>
 
@@ -14,30 +10,30 @@ import useRoadAPI from '~/api/useRoadAPI';
 import { type RoadMapPatterns, PatternSelector } from '~/types/roadmap';
 
 const props = defineProps<{
-    roadUuid:string,
-    patternSelector:PatternSelector, //哪一個套路選擇器
-    patterns:RoadMapPatterns | {}
+    roadUuid: string,
+    patternSelector: PatternSelector, //哪一個套路選擇器
+    patterns: RoadMapPatterns | {}
 }>()
 
-const { toggleMenum, showMenum } = useWatchClickOutSide()
 const { updateRoadMapPatterns } = useRoadAPI()
 
-const patternsShowText = ref<string >('?')
+const patternsShowText = ref<string>('?')
 
-function setPattern(patternKey:string, patternValue:string){
-    console.log(props.roadUuid)
-
-    patternsShowText.value = patternValue
-    toggleMenum()
-    switch(props.patternSelector){
+function setPattern() {
+    let patternKey = ''
+    Object.entries(props.patterns).find(([key, value]) => {
+        patternKey = key
+        return patternsShowText.value === value
+    })
+    switch (props.patternSelector) {
         case PatternSelector.SELECTOR1:
             updateRoadMapPatterns(props.roadUuid, {
-                pattern1:patternKey
+                pattern1: patternKey
             })
             break
         case PatternSelector.SELECTOR2:
             updateRoadMapPatterns(props.roadUuid, {
-                pattern2:patternKey
+                pattern2: patternKey
             })
             break
     }
